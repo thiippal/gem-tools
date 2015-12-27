53f931b7130a16181b638d273be07b7d164ade62
thiippal <tuomo.hiippala@iki.fi>
2015-12-27 20:32:22 +0200

    added pre-trained classifier
    Former-commit-id: 5f1e5ab7c3ea3495e959c380079ccc670ea85163

--- generator.py
+++ generator.py
@@ -1,7 +1,25 @@
 # GeM generator
 # -------------
 
+# -----------------------------
+# IMPORT THE NECESSARY PACKAGES
+# -----------------------------
+
+import cv2
+import mahotas
+import numpy as np
+
+# ----------------
 # DEFINE FUNCTIONS
-# For visual logging
-def vlog(image, title):
-    logger.debug(VisualRecord(title, image, fmt = "png"))
+# ----------------
+
+# Describe images using color statistics and Haralick textures
+
+def describe(image):
+    (means, stds) = cv2.meanStdDev(cv2.cvtColor(image, cv2.COLOR_BGR2HSV))
+    colorStats = np.concatenate([means, stds]).flatten()
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    haralick = mahotas.features.haralick(gray).mean(axis = 0)
+    
+    return np.hstack([colorStats, haralick])
